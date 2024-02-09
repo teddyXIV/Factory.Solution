c71205cfe1ebadf43d099f17ee7a61c93c3c39c4
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-09 14:39:58 -0800

    require engineer name and specialty

---
+++ Factory/Migrations/20240209223916_RequireEngineerNameAndSpecialty.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class RequireEngineerNameAndSpecialty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "Specialty",
+                keyValue: null,
+                column: "Specialty",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialty",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialty",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace Factory.Models;
 
 public class Engineer
 {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "You must provide an engineer name.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "You must provide an engineer specialty.")]
     public string Specialty { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
 }
