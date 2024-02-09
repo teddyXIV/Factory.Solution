1ef597d6dcdddef7f4020dc51bd1479f9937af7f
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-09 14:52:43 -0800

    require machine name and purpose

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace Factory.Models;
 
 public class Machine
 {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "You must provide a machine name.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "You must provide a machine purpose.")]
     public string Purpose { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
 }
