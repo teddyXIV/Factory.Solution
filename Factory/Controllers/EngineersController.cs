a240bdd52811829509b6f9fb9fcebf3a39987b37
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-09 12:35:33 -0800

    add engineer create, details, edit views + actions

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -18,4 +18,38 @@ public class EngineersController : Controller
         return View(eng);
     }
 
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer eng)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        else
+        {
+            _db.Engineers.Add(eng);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+    public ActionResult Details(int id)
+    {
+        Engineer eng = _db.Engineers
+            .Include(e => e.JoinEntities)
+            .ThenInclude(join => join.Machine)
+            .FirstOrDefault(e => e.EngineerId == id);
+        return View(eng);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Engineer eng = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
+        return View(eng);
+    }
 }
