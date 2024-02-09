c05332aed46aff27ebaf8104ac792c6b5f8f5713
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-09 13:14:57 -0800

    add controller actions and views for machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -11,4 +11,68 @@ public class MachinesController : Controller
     {
         _db = db;
     }
+
+    public ActionResult Index()
+    {
+        List<Machine> mach = _db.Machines.ToList();
+        return View(mach);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine mach)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        else
+        {
+            _db.Machines.Add(mach);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+    public ActionResult Details(int id)
+    {
+        Machine mach = _db.Machines
+            .Include(e => e.JoinEntities)
+            .ThenInclude(join => join.Engineer)
+            .FirstOrDefault(m => m.MachineId == id);
+        return View(mach);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Machine mach = _db.Machines.FirstOrDefault(m => m.MachineId == id);
+        return View(mach);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine mach)
+    {
+        _db.Machines.Update(mach);
+        _db.SaveChanges();
+        return RedirectToAction("Details", new { id = mach.MachineId });
+    }
+
+    public ActionResult Delete(int id)
+    {
+        Machine mach = _db.Machines.FirstOrDefault(m => m.MachineId == id);
+        return View(mach);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Machine mach = _db.Machines.FirstOrDefault(m => m.MachineId == id);
+        _db.Machines.Remove(mach);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
